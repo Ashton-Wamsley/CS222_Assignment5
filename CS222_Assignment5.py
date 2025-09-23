040ada67467cc84f79b1587e7da89444dd04deb1
Riddled Ocean <ashton.wamsley@bsu.edu>
2025-09-23 11:53:39 -0400

    Created first two functions to convert fahrenheit to celsius and fibonacci numbers.

---
+++ CS222_Assignment5.py
@@ -0,0 +1,20 @@
+def fahrenheitToCelsius(fahrenheit):
+    if not isinstance(fahrenheit, (int, float)):
+        raise TypeError("Tempurature has to be a number.")
+    celsius = (fahrenheit - 32) * 5.0/9.0
+    return celsius
+
+def intToFibonacci(n):
+    if (n < 0):
+        raise TypeError("Input has to be a non negative integer.")
+    if (n == 0):
+        return 0
+    elif (n == 1):
+        return 1
+    else:
+        fib1 = 0
+        fib2 = 1
+        for fib in range(2, n + 1):
+            fib1 = fib2
+            fib2 = fib1 + fib2
+            return fib2
